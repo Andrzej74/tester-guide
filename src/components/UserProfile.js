c9a13ef80dce910e9ae3ef81491fc467d884a05e
Andrzej74 <andrzej.majcher@protonmail.com>
2024-10-13 16:39:38 +0200

    update UserProfile component

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,15 +1,42 @@
-// src/components/UserProfile.js
 import React from "react";
 import { useAuth } from "../contexts/AuthContext";
+import { auth } from "../firebaseConfig";
+import { signOut, deleteUser } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const UserProfile = () => {
   const { user } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth); // Wylogowanie użytkownika
+      navigate("/"); // Przekierowanie na stronę główną po wylogowaniu
+    } catch (error) {
+      console.error("Błąd podczas wylogowania:", error);
+    }
+  };
+
+  const handleDeleteAccount = async () => {
+    if (user) {
+      try {
+        await deleteUser(user); // Usunięcie konta użytkownika
+        navigate("/"); // Przekierowanie na stronę główną po usunięciu konta
+      } catch (error) {
+        console.error("Błąd podczas usuwania konta:", error);
+      }
+    }
+  };
 
   return (
     <div>
       <h2>Profil użytkownika</h2>
       {user ? (
-        <p>Zalogowany jako: {user.email}</p>
+        <div>
+          <p>Zalogowany jako: {user.email}</p>
+          <button onClick={handleLogout}>Wyloguj się</button>
+          <button onClick={handleDeleteAccount}>Usuń konto</button>
+        </div>
       ) : (
         <p>Brak zalogowanego użytkownika</p>
       )}
