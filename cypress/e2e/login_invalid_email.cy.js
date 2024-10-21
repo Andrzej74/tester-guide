8ba1d8e600e8226f5169d1d8f1500f43edcc138d
Andrzej74 <andrzej.majcher@protonmail.com>
2024-10-21 19:31:33 +0200

    cypress test fix

--- cypress/e2e/login_invalid_email.cy.js
+++ cypress/e2e/login_invalid_email.cy.js
@@ -1,7 +1,7 @@
 describe('Invalid Email - Login', () => {
     it('should display error for invalid email format', () => {
       cy.visit('/login');
-      cy.get('#login-email').type('invalidemail');
+      cy.get('#login-email').type('non-existent@email.com');
       cy.get('#login-password').type('Test1234');
       cy.get('#login-submit').click();
       cy.get('#login-error').should('contain', 'Błąd logowania');
