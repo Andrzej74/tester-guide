f97282cdaf4d157d0c48f31759874e9889db1738
Andrzej74 <andrzej.majcher@protonmail.com>
2024-10-21 22:24:12 +0200

    added recorning cypress fails

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    screenshotOnRunFailure: true, // Zrzuty ekranów przy błędach
+    video: true, // Nagrywanie wideo podczas testów
     setupNodeEvents(on, config) {
       // konfiguracja zdarzeń
     },
