60cd462cc2cc6cb7f62a2a48863556a085ce6b8f
Andrzej74 <andrzej.majcher@protonmail.com>
2024-10-22 15:49:41 +0200

    Few styles addad

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Tester Guide</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/login" id="home-login-link">Login</Link>
+    <div className="home-container">
+      <h1 className="home-title">Welcome to the Tester Guide</h1>
+      <nav className="home-nav">
+        <ul className="home-nav-list">
+          <li className="home-nav-item">
+            <Link to="/login" id="home-login-link" className="home-link">Login</Link>
           </li>
-          <li>
-            <Link to="/register" id="home-register-link">Register</Link>
+          <li className="home-nav-item">
+            <Link to="/register" id="home-register-link" className="home-link">Register</Link>
           </li>
-          <li>
-            <Link to="/profile" id="home-profile-link">Profile</Link>
+          <li className="home-nav-item">
+            <Link to="/profile" id="home-profile-link" className="home-link">Profile</Link>
           </li>
         </ul>
       </nav>
