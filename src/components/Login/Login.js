775cee547ee61f671a79e3304447778faefdd2f9
Andrzej74 <andrzej.majcher@protonmail.com>
2024-10-27 13:52:02 +0100

    Login styling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,10 @@
+// /src/components/Login/Login.js
+
 import React, { useState } from "react";
 import { auth } from "../../firebaseConfig";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import './Login.css';
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -13,34 +16,43 @@ const Login = () => {
     e.preventDefault();
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      navigate("/dashboard"); // Przekierowanie na dashboard po zalogowaniu
+      navigate("/dashboard");
     } catch (err) {
       setError("Błąd logowania");
     }
   };
 
   return (
-    <div>
-      <h2>Logowanie</h2>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          id="login-email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          id="login-password"
-          placeholder="Hasło"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" id="login-submit">Zaloguj się</button>
-        {error && <p id="login-error">{error}</p>}
+    <div className="login-container">
+      <h2 className="login-title">Logowanie</h2>
+      <form onSubmit={handleLogin} className="login-form">
+        <div className="login-input-group">
+          <input
+            type="email"
+            id="login-email"
+            className="login-input"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            id="login-password"
+            className="login-input"
+            placeholder="Hasło"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" id="login-submit" className="login-button">
+            Zaloguj się
+          </button>
+        </div>
+        {error && <p id="login-error" className="login-error">{error}</p>}
       </form>
-      <p>Nie masz konta? <a href="/register" id="login-register-link">Zarejestruj się</a></p> {/* Link do rejestracji */}
+      <div className="login-register-container">
+        <p className="login-register-text">Nie masz konta?</p>
+        <a href="/register" id="login-register-link" className="login-register-link">Zarejestruj się</a>
+      </div>
     </div>
   );
 };
